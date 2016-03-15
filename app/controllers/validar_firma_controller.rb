5bb44b7e1953f7109cdd36eaa1fbe50bcd6f9258
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-03-15 19:14:56 -0300

    fix error codes on validarFirma

--- app/controllers/validar_firma_controller.rb
+++ app/controllers/validar_firma_controller.rb
@@ -2,14 +2,16 @@ class ValidarFirmaController < ApplicationController
   def index
     if params[:mensaje] && params[:hash]
       sha256 = Digest::SHA256.new
-      hash = sha256.hexdigest params[:mensaje]
+      unless hash = sha256.hexdigest params[:mensaje]
+        render nothing: true, status: 500
+      end
       if hash.downcase == (params[:hash].downcase)
         render :json => true
       else
         render :json => false
       end
     else
-      render nothing: true
+      render nothing: true, status: 400
     end
   end
 end
