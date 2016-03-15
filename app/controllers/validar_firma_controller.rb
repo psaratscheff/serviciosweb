2abf2fd470c734de41c2d204865e9cf1a618a0be
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-03-15 19:25:53 -0300

    fix 500 error

--- app/controllers/validar_firma_controller.rb
+++ app/controllers/validar_firma_controller.rb
@@ -2,9 +2,10 @@ class ValidarFirmaController < ApplicationController
   def index
     if params[:mensaje] && params[:hash]
       sha256 = Digest::SHA256.new
-      unless hash = sha256.hexdigest params[:mensaje]
+      if !sha256.hexdigest params[:mensaje]
         render nothing: true, status: 500
       end
+      hash = sha256.hexdigest params[:mensaje]
       if hash.downcase == (params[:hash].downcase)
         render :json => true
       else
